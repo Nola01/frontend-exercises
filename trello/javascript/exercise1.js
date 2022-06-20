5a21f77d06d20aadf9e9f83c120a588134431076
Juanma Nolasco <juanmaria.nolasco@bosonit.com>
2022-06-20 09:53:45 +0200

    Javascript - Exercise 1

---
+++ trello/javascript/exercise1.js
@@ -0,0 +1,25 @@
+/*
+    Ejercicio 1
+    Dado un array de objetos, obtener el objeto con el id 3
+    const arrNames = [
+    {id: 1, name: 'Pepe'},
+    {id: 2, name: 'Juan'},
+    {id: 3, name: 'Alba'},
+    {id: 4, name: 'Toby'},
+    {id: 5, name: 'Lala'}
+    ]
+
+*/
+
+const arrNames = [
+    {id: 1, name: 'Pepe'},
+    {id: 2, name: 'Juan'},
+    {id: 3, name: 'Alba'},
+    {id: 4, name: 'Toby'},
+    {id: 5, name: 'Lala'}
+]
+
+const name3 = arrNames.filter(name => name.id === 3);
+
+console.log(name3);
+
